28ca2c21439c159de8242c985cf82bf19057e044
yashsuthar194 <yashsuthar352@gmail.com>
2026-03-01 23:44:06 +0530

    Format issue fixed

--- prisma/migrations/20260224165005_driver_format_id/migration.sql
+++ prisma/migrations/20260224165005_driver_format_id/migration.sql
@@ -8,20 +8,6 @@
 -- AlterTable
 ALTER TABLE "driver" ADD COLUMN     "formated_id" TEXT NOT NULL;
 
--- 1. Create the function
-CREATE OR REPLACE FUNCTION set_driver_display_id()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.formated_id := 'DRI' || LPAD(NEW.id::text, 7, '0');
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- 2. Create the trigger
-CREATE TRIGGER trg_set_driver_display_id
-BEFORE INSERT ON "driver"
-FOR EACH ROW EXECUTE FUNCTION set_driver_display_id();
-
 
 -- CreateIndex
 CREATE UNIQUE INDEX "driver_formated_id_key" ON "driver"("formated_id");
