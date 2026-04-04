b596e3e03c7f5692bf8cee5b70a8af9354366a1f
Shahvinit3113 <shahvinit315@gmail.com>
2026-04-05 00:27:44 +0530

    changes

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -5,10 +5,17 @@ import {
   OnModuleDestroy,
   Logger,
 } from '@nestjs/common';
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+const adapter = new PrismaPg(pool);
 
 @Injectable()
 export class PrismaService
